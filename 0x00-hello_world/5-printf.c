b4acac9af15c013da036cf07807466cce769eaab
SYZBA1 <samyos3560@gmail.comgit config --global user.emailsamyo9090@gmail.com>
2023-09-07 19:14:15 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 /**
- * main - Entry point 
+ * main - Prints "eith proper grammer, but the outcome is
+ *  a piece of art," followed by new line.  
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
+
 int main(void)
+
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
