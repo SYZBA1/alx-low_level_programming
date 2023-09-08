def6cc5555ddf69aae781c123b7773b14aece6e2
SYZBA1 <samyos3560@gmail.comgit config --global user.emailsamyo9090@gmail.com>
2023-09-07 19:51:37 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Printe the size of various typesa based on 
+ * the computer it is compiled and run on..
+ * Return: Always 0.
  */
+
 int main(void)
 {
-	printf("Size of char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d btye(s)\n", sizeof(long long int));
-	printf("Size of a float: %d btye(s)\n", sizeof(float));
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu btye(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu btye(s)\n", sizeof(float));
 	return (0);
 }
