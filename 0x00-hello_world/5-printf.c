bf8200e922b406cdf6d503a9d5777402560e533b
SYZBA1 <samyos3560@gmail.comgit config --global user.emailsamyo9090@gmail.com>
2023-09-11 03:53:16 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 
 /**
  * main - Prints "with proper grammer, but the outcome is
- *  a piece of art," followed by new line.  
+ * a piece of art," followed by a new line.
  * Return: Always 0.
  */
 
