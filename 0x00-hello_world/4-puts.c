c585199a8b8ba937a2c688fa132967562df31c7f
SYZBA1 <samyos3560@gmail.comgit config --global user.emailsamyo9090@gmail.com>
2023-09-11 02:39:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
+
 /**
- * main - prints "Programming is like building a multilingual puzzle,
- * followed by a newline."
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline."
  * Return: Always 0.
  */
+
+
 int main(void)
+
+
 {
-	puts(\n"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
