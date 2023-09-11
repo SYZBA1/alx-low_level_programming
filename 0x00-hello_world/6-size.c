e06de0e3667a43fb1e836522019690e2f787c5be
SYZBA1 <samyos3560@gmail.comgit config --global user.emailsamyo9090@gmail.com>
2023-09-11 06:01:12 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	printf("Size of char: %zu byte(s)\n", sizeof(char));
-
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu btye(s)\n", sizeof(long long int));
