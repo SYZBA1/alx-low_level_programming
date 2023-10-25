be15d6362485240d1d62cad211489eaaa45d9894
SYZBA1 <samyos9090@gmail.com>
2023-10-24 19:30:22 -0700

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked lists.
+ * @v: linked list of type listint_t to traverse.
+ *
+ * Return: number of nodes.
+ */
+size_t listint_len(const listint_t *v)
+{
+	size_t num = 0;
+
+	while (v)
+ 	{
+	num++;
+	v = v->next;
+	}
+
+	return (num);
+}
