563ee296be52382a90324a8608c0141d7eab8ec4
SYZBA1 <samyos9090@gmail.com>
2023-10-24 19:54:42 -0700

    updated

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a linked list.
+ * add_nodeint - adds a new node at the beginning of a linked list
  * @head: pointer to the first node in the list.
- * @m: data to insert in that new node.
+ * @n: data to insert in that new node.
  *
  * Return: pointer to the new node, or NULL if it fails.
  */
-listint_t *add_nodeint(listint_t **head, const int m)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int m)
 	if (!new)
 	return (NULL);
 
-	new->m = m;
+	new->n = n;
 	new->next = *head;
 	*head = new;
 
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a node at the end of a linked list.
- * @head: pointer to the first element in the list.
- * @u: data to insert in the new element.
+ * add_nodeint_end - adds a node at the end of a linked list
+ * @head: pointer to the first element in the list
+ * @n: data to insert in the new element
  *
- * Return: pointer to the new node, or NULL if it fails.
+ * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint_end(listint_t **head, const int u)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *temp = *head;
@@ -16,7 +16,7 @@ listint_t *add_nodeint_end(listint_t **head, const int u)
 	if (!new)
 	return (NULL);
 
-	new->u = u;
+	new->n = n;
 	new->next = NULL;
 
 	if (*head == NULL)
@@ -32,3 +32,4 @@ listint_t *add_nodeint_end(listint_t **head, const int u)
 
 	return (new);
 }
+
